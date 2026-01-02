0f4740a5bcff2a066252619d37af6cc6c4fcbef0
ngDuyHung <nguyenduyhung2456@gmail.com>
2026-01-02 14:37:17 +0700

    update home layout news

--- resources/views/client/home.blade.php
+++ resources/views/client/home.blade.php
@@ -612,7 +612,7 @@
    </script>
  </section>
 
- <section class="section awe-section-7">
+ <section class="section awe-section-8">
    <div class="section_product section_product_2 section_product_tag section">
      <div class="container">
        <div
@@ -892,141 +892,7 @@
      </div>
    </div>
  </section>
- <section class="section awe-section-14">
-   <section class="section_blog section">
-     <div class="container">
-
-       <div class='row'>
-         <div class='col-12 col-lg-6'>
-           <div class='rounded pt-3' style='background-color: var(--body-background); padding: 20px'>
-             <div class="title_module_main heading-bar d-flex justify-content-between align-items-center">
-               <h2 class="heading-bar__title ">
-                 <a class='link' href="tin-khuyen-mai" title="THÔNG TIN KHUYẾN MÃI">THÔNG TIN KHUYẾN MÃI</a>
-               </h2>
-             </div>
-
-             <div class="blog-list">
-               <div class="blogwp clearfix media  ">
-
-                 <a class="image-blog  text-center mr-3" href="/tra-sau-fundiin"
-                   title="Fundiin x MemoryZone: Lên đời công nghệ - Trả sau nhẹ nhàng, nhận ưu đãi đến 50K!">
-                   <img loading="lazy" class="img-fluid"
-                     src="https://bizweb.dktcdn.net/100/329/122/articles/mmz-fundiin-thumbnailblog-600x400px.jpg?v=1761626535663"
-                     alt="Fundiin x MemoryZone: Lên đời công nghệ - Trả sau nhẹ nhàng, nhận ưu đãi đến 50K!">
-                 </a>
-
-                 <div class="content_blog clearfix media-body ">
-                   <h3 class='mt-0'>
-                     <a class='link' href="/tra-sau-fundiin"
-                       title="Fundiin x MemoryZone: Lên đời công nghệ - Trả sau nhẹ nhàng, nhận ưu đãi đến 50K!">Fundiin
-                       x MemoryZone: Lên đời công nghệ - Trả sau nhẹ nhàng, nhận ưu đãi đến 50K!</a>
-                   </h3>
-                   <div class="media">
-                     <div class="media-body d-flex flex--wrap align-items-center">
-
-
-                       <div class='text-muted mt-0'>
-
-
-                         Thứ Ba,
-                         28/10/2025
-                       </div>
-                     </div>
-                   </div>
-
-                   <p class="justify lead d-none d-md-block ">
-                     Fundiin kết hợp cùng MemoryZone mang đến chương trình ưu đãi cực hấp dẫn dành cho khách hàng yêu
-                     công nghệ! Mua sắm laptop, PC,...
-
-                     <a class="button_custome_35 link" href="/tra-sau-fundiin" title="Đọc tiếp">Đọc tiếp</a>
-
-                   </p>
-
-                 </div>
-               </div>
-
-
-             </div>
-             <div class="text-center mt-4">
-               <a href="tin-khuyen-mai" title="Xem tất cả"
-                 class="btn btn-icon w-100 justify-content-center btn-secondary">
-                 Xem tất cả
-
-                 <svg class="icon">
-                   <use xlink:href="#icon-arrow" />
-                 </svg>
-               </a>
-             </div>
-           </div>
-         </div>
-         <div class='col-12 col-lg-6'>
-           <div class='rounded pt-3' style='background-color: var(--body-background); padding: 20px'>
-             <div class="title_module_main heading-bar d-flex justify-content-between align-items-center">
-               <h2 class="heading-bar__title ">
-                 <a class='link' href="tin-tuc" title="KINH NGHIỆM HAY - MẸO VẶT">KINH NGHIỆM HAY - MẸO VẶT</a>
-               </h2>
-             </div>
-
-
-             <div class="blog-list">
-               <div class="blogwp clearfix media  ">
-
-                 <a class="image-blog  text-center mr-3" href="/top-5-dong-laptop-asus-tot-nhat-2026"
-                   title="Khám phá top 5 laptop ASUS tốt nhất trong năm 2026">
-                   <img loading="lazy" class="img-fluid"
-                     src="https://bizweb.dktcdn.net/100/329/122/articles/laptop-asus-tot-nhat.png?v=1761886845467"
-                     alt="Khám phá top 5 laptop ASUS tốt nhất trong năm 2026">
-                 </a>
-
-                 <div class="content_blog clearfix media-body ">
-                   <h3 class='mt-0'>
-                     <a class='link' href="/top-5-dong-laptop-asus-tot-nhat-2026"
-                       title="Khám phá top 5 laptop ASUS tốt nhất trong năm 2026">Khám phá top 5 laptop ASUS tốt nhất
-                       trong năm 2026</a>
-                   </h3>
-                   <div class="media">
-                     <div class="media-body d-flex flex--wrap align-items-center">
-
-
-                       <div class='text-muted mt-0'>
-
-
-                         Thứ Sáu,
-                         31/10/2025
-                       </div>
-                     </div>
-                   </div>
-
-                   <p class="justify lead d-none d-md-block ">Nếu bạn đang tìm kiếm chiếc laptop ASUS tốt nhất để đồng
-                     hành cùng công việc, học tập hay giải trí thì đây chính là...
-
-                     <a class="button_custome_35 link" href="/top-5-dong-laptop-asus-tot-nhat-2026"
-                       title="Đọc tiếp">Đọc tiếp</a>
-
-                   </p>
-
-                 </div>
-               </div>
-
-
-             </div>
-             <div class="text-center mt-4">
-               <a href="tin-tuc" title="Xem tất cả" class="btn btn-icon w-100 justify-content-center btn-secondary">
-                 Xem tất cả
-
-                 <svg class="icon">
-                   <use xlink:href="#icon-arrow" />
-                 </svg>
-               </a>
-             </div>
-
-           </div>
-         </div>
-       </div>
-
-     </div>
-   </section>
- </section>
+ @include('layouts.partials.client.news')
 
 
  <script>
